79542acd8e226040f70462f7255511133f1805f7
Thiago Mourão <tobrasil@gmail.com>
2026-06-25 16:41:29 -0300

    feat(palestras): URLs em /palestra_publica (compat WP) + redirect 301 de /palestras

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => view('pages.inicio'))->name('home');
 
-Route::get('/palestras', [PalestraController::class, 'index'])->name('palestras.index');
-Route::get('/palestras/{slug}', [PalestraController::class, 'show'])->name('palestras.show');
+Route::get('/palestra_publica', [PalestraController::class, 'index'])->name('palestras.index');
+Route::get('/palestra_publica/{slug}', [PalestraController::class, 'show'])->name('palestras.show');
+
+// Compat: URLs antigas (WP/divulgação) → 301 para as novas, preservando o slug.
+Route::permanentRedirect('/palestras', '/palestra_publica');
+Route::get('/palestras/{slug}', fn (string $slug) => redirect()->route('palestras.show', ['slug' => $slug], 301));
 
 Route::get('/palestrantes', [PalestranteController::class, 'index'])->name('palestrantes.index');
 Route::get('/palestrantes/{slug}', [PalestranteController::class, 'show'])->name('palestrantes.show');
